2cea1f70082e06cf9d97f50000ecf864b1c26c1f
toma09to <yamto0916@gmail.com>
2022-12-11 00:00:32 +0900

    Add /branches /leaves

--- src/main.go
+++ src/main.go
@@ -9,6 +9,17 @@ func main() {
     e := echo.New()
 
     e.GET("/trees", routing.GetTree)
+    e.POST("/trees", routing.PostTree)
+    e.PUT("/trees/:id", routing.PutTree)
+    e.DELETE("/trees/:id", routing.DeleteTree)
+    e.GET("/branches", routing.GetBranch)
+    e.POST("/branches", routing.PostBranch)
+    e.PUT("/branches/:id", routing.PutBranch)
+    e.DELETE("/branches/:id", routing.DeleteBranch)
+    e.GET("/leaves", routing.GetLeaf)
+    e.POST("/leaves", routing.PostLeaf)
+    e.PUT("/leaves/:id", routing.PutLeaf)
+    e.DELETE("/leaves/:id", routing.DeleteLeaf)
 
     e.Logger.Fatal(e.Start(":8080"))
 }
--- src/routing/main.go
+++ src/routing/main.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "github.com/labstack/echo"
 
-    "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
 
     "src/databases"
@@ -52,11 +51,160 @@ func GetTree(c echo.Context) error {
     db := databases.GormConnect()
     defer db.Close()
 
-    return c.String(200, "")
-}
-
-func searchTree(db *gorm.DB) []Tree {
     var trees []Tree
     db.Find(&trees)
-    return trees
+
+    return c.JSON(200, trees)
+}
+
+func PostTree(c echo.Context) error {
+    db:= databases.GormConnect()
+    defer db.Close()
+
+    tree := new(Tree)
+    if err := c.Bind(tree); err != nil {
+        return err
+    }
+
+    db.Create(&tree)
+    db.Where(&tree).Find(&tree)
+
+    return c.JSON(201, tree)
+}
+
+func PutTree(c echo.Context) error {
+    db := databases.GormConnect()
+    defer db.Close()
+
+    tree := new(Tree)
+    id := c.Param("id")
+    db.Where("tree_id = ?", id).Find(&tree)
+
+    updateValues := new(Tree)
+    if err := c.Bind(updateValues); err != nil {
+        return err
+    }
+
+    db.Model(&tree).Updates(updateValues)
+    db.Where(&tree).Find(&tree)
+
+    return c.JSON(200, tree)
+}
+
+func DeleteTree(c echo.Context) error {
+    db:= databases.GormConnect()
+    defer db.Close()
+    
+    id := c.Param("id")
+    db.Delete(&Tree{}, "tree_id = ?", id)
+
+    return c.NoContent(204)
+}
+
+func GetBranch(c echo.Context) error {
+    db := databases.GormConnect()
+    defer db.Close()
+
+    var branches []Branch
+    db.Find(&branches)
+
+    return c.JSON(200, branches)
+}
+
+func PostBranch(c echo.Context) error {
+    db:= databases.GormConnect()
+    defer db.Close()
+
+    branch := new(Branch)
+    if err := c.Bind(branch); err != nil {
+        return err
+    }
+
+    db.Create(&branch)
+    db.Where(&branch).Find(&branch)
+
+    return c.JSON(201, branch)
+}
+
+func PutBranch(c echo.Context) error {
+    db := databases.GormConnect()
+    defer db.Close()
+
+    branch := new(Branch)
+    id := c.Param("id")
+    db.Where("branch_id = ?", id).Find(&branch)
+
+    updateValues := new(Branch)
+    if err := c.Bind(updateValues); err != nil {
+        return err
+    }
+
+    db.Model(&branch).Updates(updateValues)
+    db.Where(&branch).Find(&branch)
+
+    return c.JSON(200, branch)
+}
+
+func DeleteBranch(c echo.Context) error {
+    db:= databases.GormConnect()
+    defer db.Close()
+    
+    id := c.Param("id")
+    db.Debug().Delete(&Branch{}, "branch_id = ?", id)
+
+    return c.NoContent(204)
+}
+
+func GetLeaf(c echo.Context) error {
+    db := databases.GormConnect()
+    defer db.Close()
+
+    var leaves []Leaf
+    db.Find(&leaves)
+
+    return c.JSON(200, leaves)
+}
+
+func PostLeaf(c echo.Context) error {
+    db:= databases.GormConnect()
+    defer db.Close()
+
+    leaf := new(Leaf)
+    if err := c.Bind(leaf); err != nil {
+        return err
+    }
+
+    db.Create(&leaf)
+    db.Where(&leaf).Find(&leaf)
+
+    return c.JSON(201, leaf)
+}
+
+func PutLeaf(c echo.Context) error {
+    db := databases.GormConnect()
+    defer db.Close()
+
+    leaf := new(Leaf)
+    id := c.Param("id")
+    db.Where("leaf_id = ?", id).Find(&leaf)
+
+    updateValues := new(Leaf)
+    if err := c.Bind(updateValues); err != nil {
+        return err
+    }
+
+    db.Model(&leaf).Updates(updateValues)
+    db.Where(&leaf).Find(&leaf)
+
+    return c.JSON(200, leaf)
+}
+
+func DeleteLeaf(c echo.Context) error {
+    db:= databases.GormConnect()
+    defer db.Close()
+    
+    id := c.Param("id")
+    db.Debug().Delete(&Leaf{}, "leaf_id = ?", id)
+
+    return c.NoContent(204)
 }
